9b6898982c2db1c4752df1116af7eea1fe1719f8
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-11 20:15:56 +0530

    removed module

--- pos-gui/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module tech.altier.posgui {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens tech.altier.posgui to javafx.fxml;
-    exports tech.altier.posgui;
-}
